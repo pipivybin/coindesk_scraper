1eec9eaebd0ec65d6304b5fc48c3fa6ca5481429
Ming Duan <11203duan@gmail.com>
2019-04-08 21:47:26 +0000

    Updated Return Value for Pages not Readable

--- lib/coindesk_scraper/coindesk_articles.rb
+++ lib/coindesk_scraper/coindesk_articles.rb
@@ -31,22 +31,22 @@
     @top_article.time = doc.css("div#top-article div.meta div.time time").attr("datetime").value
     @top_article.url = doc.css("div#top-article a.top-article").attr("href").value
     
-    common1 = doc.css("div#featured-articles")
+    feature = doc.css("a.feature")
     
-    common1.css("div.meta h3").each do |x|
+    feature.each do |x|
       @featured_articles = self.new
-      @featured_articles.title = x.text
-      @featured_articles.time = common1.css("div.meta div.time time").attr("datetime").value
-      @featured_articles.url = common1.css("a.feature").attr("href").value
+      @featured_articles.title = x.css("div.meta h3").text
+      @featured_articles.time = x.css("div.meta div.time time").attr("datetime").value
+      @featured_articles.url = x.attr("href")
       end
     
-    common2 = doc.css("div#article-streams")
+    streams = doc.css("a.stream-article")
     
-    common2.css("div.meta h3").each do |x|
+    streams.each do |x|
       @article_streams = self.new
-      @article_streams.title = x.text
-      @article_streams.time = common2.css("div.meta div.time time").attr("datetime").value
-      @article_streams.url = common2.css("a.stream-article").attr("href").value
+      @article_streams.title = x.css("div.meta h3").text
+      @article_streams.time = x.css("div.meta div.time time").attr("datetime").value
+      @article_streams.url = x.attr("href")
     end
 
  end
@@ -54,15 +54,20 @@
  def self.chosen_article(input)
   chosen = input.to_i
   link = self.all[chosen-1].url
-  binding.pry
   doc = Nokogiri::HTML(open(link))
-  puts "#{doc.css("div.meta h1").text}"
-  puts ""
-  puts "#{doc.css("i.far.fa-clock span").text}"
-  puts ""
-  puts "#{doc.css("section.article-content").text}"
-  puts ""
-  
+  if doc.css("div.meta h1").text == "" || nil
+    puts "#{self.all[chosen-1].title}"
+    puts ""
+    puts "To find out more, please click the link: #{link}"
+    puts ""
+  else
+    puts "#{doc.css("div.meta h1").text}"
+    puts ""
+    puts "#{doc.css("i.far.fa-clock span").text}"
+    puts ""
+    puts "#{doc.css("section.article-content").text}"
+    puts ""
+  end
  end
   
     
