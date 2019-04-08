73cc503f407ed88c929942563bffa5d2ceb77b68
Ming Duan <11203duan@gmail.com>
2019-04-08 21:00:57 +0000

    Added Article Selection Method

--- lib/coindesk_scraper/cli.rb
+++ lib/coindesk_scraper/cli.rb
@@ -1,15 +1,23 @@
+require 'pry'
+
 class CoindeskScraper::CLI
   
   def call 
     puts "Today's top 10 news are here!"
+    make_list
     list
     menu
   end
   
+  def make_list
+    CoindeskScraper::Articles.make_articles
+  end
+  
   def list
     CoindeskScraper::Articles.all.each_with_index do |one_article, index|
-      puts "No #{index+1}. one_article.title - one_article.time - one_article.url"
+      puts "No #{index+1}. #{one_article.title} - #{one_article.time} - #{one_article.url}"
     end
+    puts ""
   end
   
   def menu
@@ -18,16 +26,23 @@ class CoindeskScraper::CLI
       puts "- You can choose a number from the list to read more"
       puts "- You can review the list by typing list"
       puts "- Or you can type exit to exit"
+      puts ""
       input = gets.strip.chomp.downcase
     
       if input == "exit"
         puts "Thank you for visiting us! You have a wonderful day!"
+        CoindeskScraper::Articles.clear_all
         break
       elsif (1..10) === input.to_i
         puts "Here's No.#{input.to_i} article for you!"
+        puts ""
+        CoindeskScraper::Articles.chosen_article(input)
       elsif input == "list"
         puts list
-      else puts "Please input a number between 1 - 10 or type exit."
+        
+      else 
+        puts "Please input a number between 1 - 10 or type exit."
+        puts ""
         
       end
     end
--- lib/coindesk_scraper/coindesk_articles.rb
+++ lib/coindesk_scraper/coindesk_articles.rb
@@ -6,17 +6,6 @@
  @@all = []
  attr_accessor :title, :time, :url
   
-  # def self.get_page
-  #   html = open("https://www.coindesk.com/")
-  #   doc = Nokogiri::HTML(html)
-  # end
-  
-  # def self.scrape
-  #   @article = self.new
-  #   doc = self.get_page
-    
-  # end
-  
   def initialize
     save
   end
@@ -29,33 +18,51 @@
     @@all
   end
   
+  def self.clear_all
+    self.all.clear
+  end
+  
   def self.make_articles
     html = open("https://www.coindesk.com/")
     doc = Nokogiri::HTML(html)
     
     @top_article = self.new
-    @top-article.title = doc.css("div#top-article div.meta h1").text
-    @top-article.time = doc.css("div#top-article div.meta div.time time").attr("datetime").value
-    @top-article.url = doc.css("div#top-article a.top-article").attr("href").value
+    @top_article.title = doc.css("div#top-article div.meta h1").text
+    @top_article.time = doc.css("div#top-article div.meta div.time time").attr("datetime").value
+    @top_article.url = doc.css("div#top-article a.top-article").attr("href").value
     
     common1 = doc.css("div#featured-articles")
     
-    common1.each do |x|
+    common1.css("div.meta h3").each do |x|
       @featured_articles = self.new
-      @featured_articles.title = x.css("div.meta h3").text
-      @featured_articles.time = x.css("div.meta div.time time").attr("datetime").value
-      @featured_articles.url = x.css("a.top-article").attr("href").value
+      @featured_articles.title = x.text
+      @featured_articles.time = common1.css("div.meta div.time time").attr("datetime").value
+      @featured_articles.url = common1.css("a.feature").attr("href").value
       end
     
     common2 = doc.css("div#article-streams")
     
-    common2.each do |x|
+    common2.css("div.meta h3").each do |x|
       @article_streams = self.new
-      @article_streams.title = x.css("div.meta h3").text
-      @article_streams.time = x.css("div.meta div.time time").attr("datetime").value
-      @article_streams.url = x.css("a.stream-article").attr("href").value
+      @article_streams.title = x.text
+      @article_streams.time = common2.css("div.meta div.time time").attr("datetime").value
+      @article_streams.url = common2.css("a.stream-article").attr("href").value
     end
-   
+
+ end
+ 
+ def self.chosen_article(input)
+  chosen = input.to_i
+  link = self.all[chosen-1].url
+  
+  doc = Nokogiri::HTML(open(link))
+  puts "#{doc.css("div.meta h1").text}"
+  puts ""
+  puts "#{doc.css("i.far.fa-clock span").text}"
+  puts ""
+  puts "#{doc.css("section.article-content").text}"
+  puts ""
+  
  end
   
     
