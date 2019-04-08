8ca9165e78ddf8c570029f6954d6b092404f322d
Ming Duan <11203duan@gmail.com>
2019-04-08 18:25:09 +0000

    Updated Some Methods

--- lib/coindesk_scraper/cli.rb
+++ lib/coindesk_scraper/cli.rb
@@ -12,16 +12,19 @@ class CoindeskScraper::CLI
   
   def menu
     loop do
-      puts "Tell me what you would like to do next: choose a number from the list to read more or exit?"
-      input = gets.strip.chomp
+      puts "Tell me what you would like to do next:"
+      puts "- You can choose a number from the list to read more"
+      puts "- You can review the list by typing list"
+      puts "- Or you can type exit to exit"
+      input = gets.strip.chomp.downcase
     
       if input == "exit"
         puts "Thank you for visiting us! You have a wonderful day!"
         break
-        
       elsif (1..10) === input.to_i
         puts "Here's No.#{input.to_i} article for you!"
-        
+      elsif input == "list"
+        puts list
       else puts "Please input a number between 1 - 10 or type exit."
         
       end
